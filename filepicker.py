81948bbb272f62e81c1d8e88734cf0b23443ef28
Russell Cohen <rcoh@mit.edu>
2012-12-21 01:09:56 -0500

    Sketch of interacting with filepicker directly from python. Snags cookies from chrome.

--- filepicker.py
+++ filepicker.py
@@ -1,64 +1,49 @@
-#!/usr/bin/env python
+"""
+A python library for accessing Filepicker.io
+"""
 
-from sys import argv, exit
-from time import time
+import urllib, urllib2
+import json
+import cookielib
+from cookies import build_cookiejar
 
-from fuse import FUSE, Operations, LoggingMixIn
+FP_API_KEY = "YOURAPIKEY_HERE"
+FP_HOSTNAME = "www.filepicker.io"
+FP_BASEURL = "https://" + FP_HOSTNAME + "/"
+REQUEST_CODE_AUTH = 600
+REQUEST_CODE_GETFILE = 601
+REQUEST_CODE_SAVEFILE = 602
+REQUEST_CODE_GETFILE_LOCAL = 603
 
-class FilePicker(LoggingMixIn, Operations):
-  def __init__(self, host, path='.'):
-    pass
-  def chmod(self, path, mode):
-    pass
+def api_key_dict():
+  return { 'app': {'apikey': FP_API_KEY} }
 
-  def chown(self, path, uid, gid):
-    pass
+def js_session():
+  return api_key_dict()
 
-  def create(self, path, mode):
-    pass
+def js_session_mimetypes(mimetypes):
+  base_dict = api_key_dict()
+  base_dict['mimetypes'] = mimetypes
+  return base_dict
 
-  def destroy(self, path):
-    pass
+def get_path(path, mimetypes):
+  safe_path = urllib.quote_plus(path)
+  base_url = FP_BASEURL + "api/path" + safe_path 
+  query = {'format': 'info', 'js_session':
+      json.dumps(js_session_mimetypes(mimetypes)) }
 
-  def getattr(self, path, fh=None):
-    pass
-  def mkdir(self, path, mode):
-    pass
-
-  def read(self, path, size, offset, fh):
-    pass
-
-  def readdir(self, path, fh):
-    pass
-
-  def readlink(self, path):
-    pass
+  url = "%s?%s" % (base_url, urllib.urlencode(query))
+  print "url: ", url
+  return url
   
-  def rename(self, old, new):
-    pass
+realurl = \
+"""https://www.filepicker.io/api/path/Dropbox/6.01%20Design/?js_session=%257B%2522apikey%2522%253A%2522l5uQ3k7FQ5GoYCHyTdZV%2522%252C%2522mimetypes%2522%253A%255B%2522image%252F*%2522%255D%252C%2522persist%2522%253Afalse%252C%2522version%2522%253A%2522v1%2522%252C%2522storeLocation%2522%253Anull%257D&format=info"""
 
-  def rmdir(self, path):
-    pass
+builturl = get_path("/Dropbox/", "")
 
-  def symlink(self, target, source):
-    pass
-
-  def truncate(self, path, length, fh=None):
-    pass
-
-  def unlink(self, path):
-    pass
-
-  def utimens(self, path, times=None):
-    pass
-
-  def write(self, path, data, offset, fh):
-    pass
-
-
-if __name__ == '__main__':
-  if len(argv) != 3:
-    print('usage: %s <mountpoint>' % argv[0])
-    exit(1)
-
-  fuse = FUSE(FilePicker(argv[1]), argv[2], foreground=True, nothreads=True)
+cj = cookielib.CookieJar()
+build_cookiejar('.filepicker.io', cj)
+print cj
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+r = opener.open(builturl)
+print r.read()
